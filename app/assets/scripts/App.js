c57d9e251bc8d5932820916c3ffa038b1d21c2ae
sunnymesa <huang0503@hotmail.com>
2017-08-08 14:46:49 -0700

    header change, still need to fix

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,21 +1,4 @@
-var $ = require ("jquery");
-// var Person = require("./modules/Person"); /* this is node.js, use import below from es6 */
-import Person from "./modules/Person";
 
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name +  "owns taxes");
-  }
-}
+import MobileMenu from "./modules/MobileMenu.js";
 
-
-// alert("ABCD");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+
+class MobileMenu {
+  constructor() {
+    // alert ("Testing from mobile menu");
+
+    // $(".site-header__menu-icon").click(function() {
+    //   console.log("top right icon was click");
+    // });
+
+    this.siteHeader = $("site-header");
+    this.menuIcon = $("site-header__menu-icon");
+    this.menuContent = $("site-header__menu-content");
+    this.events();
+  }
+
+  events() {
+    this.menuIcon.click(this.toggleTheMenu.bind(this));
+  }
+
+  toggleTheMenu() {
+    // console.log("icon was clicked");
+    this.menuContent.toggleClass("site-header__menu-content--is-visible");
+    this.siteHeader.toggleClass("site-header--is-expanded");
+    this.menuIcon.toggleClass("site-header__menu-icon--close-x");
+
+    console.log("hello");
+  }
+}
+
+export default MobileMenu;
