55341cef83d3505bd1519971502ee3b594dae3d4
sunnymesa <huang0503@hotmail.com>
2017-08-09 15:21:38 -0700

    done sticky header, add jquery smooth scolling

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,8 @@ import $ from "jquery";
 
 import MobileMenu from "./modules/MobileMenu.js";
 import RevealOnScroll from "./modules/RevealOnScoll.js";
+import StickyHeader from "./modules/StickyHeader.js";
+
 
 var mobileMenu = new MobileMenu();
 
@@ -9,3 +11,6 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 // testimonial
 new RevealOnScroll($(".testimonials"), "60%");
+
+
+var stickyHeader = new StickyHeader();
