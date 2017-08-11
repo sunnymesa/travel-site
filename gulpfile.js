47c471f5702b8749bd779731c201be5be6c15d17
sunnymesa <huang0503@hotmail.com>
2017-08-11 14:15:45 -0700

    complete mondernizr, flexbox

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 require("./gulp/tasks/styles");
 require("./gulp/tasks/watch");
-require("./gulp/tasks/sprites.js");
-require("./gulp/tasks/scripts.js");
+require("./gulp/tasks/sprites");
+require("./gulp/tasks/scripts");
+require("./gulp/tasks/modernizr");
+
 
