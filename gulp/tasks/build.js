472541fd9ebc620441d6293f36ae1ca37da0b64c
sunnymesa <huang0503@hotmail.com>
2017-08-16 15:37:11 -0700

    complete build dist

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,10 +2,39 @@ var gulp = require('gulp');
 var imagemin = require("gulp-imagemin");
 var del =require("del");
 var usemin = require("gulp-usemin");
+var rev = require("gulp-rev");
+var uglify = require("gulp-uglify");
+var cssnano = require("gulp-cssnano");
+var browserSync = require('browser-sync');
 
-gulp.task("deleteDistFolder", function() {
+
+gulp.task("previewDist", function() {
+    browserSync.init({
+      notify: false,
+      server: {
+        baseDir: "dist"
+      }
+    });
+});
+
+
+gulp.task("deleteDistFolder", ['icons'], function() {
   return del("./dist");
 })
+
+gulp.task("copyGeneralFile", ['deleteDistFolder'], function() {
+  var pathsToCopy = [
+    "./app/**/*",
+    "!./app/index.html",
+    "!./app/assets/images/**",
+    "!./app/assets/styles/**",
+    "!./app/assets/scripts/**",
+    "!./app/temp/",
+    "!./app/temp/**"
+  ];
+  return gulp.src(pathsToCopy)
+  .pipe(gulp.dest("./dist"));
+});
 
 gulp.task("optimizeImages", ["deleteDistFolder"], function() {
   return gulp.src([
@@ -21,10 +50,24 @@ gulp.task("optimizeImages", ["deleteDistFolder"], function() {
   .pipe(gulp.dest("./dist/assets/images"));
 });
 
-gulp.task("usemin", ["deleteDistFolder"], function() {
+gulp.task("useminTrigger", ["deleteDistFolder"], function() {
+  gulp.start("usemin");
+});
+gulp.task("usemin", ["styles", "scripts"], function() {
   return gulp.src("./app/index.html")
-  .pipe(usemin())
+  .pipe(usemin({
+    css:[function() {
+      return rev();
+    }, function() {
+      return cssnano();
+    }],
+    js: [function(){
+      return rev();
+    }, function() {
+      return uglify();
+    }]
+  }))
   .pipe(gulp.dest("./dist"));
 })
 
-gulp.task('build', ["deleteDistFolder", 'optimizeImages', 'usemin']);
+gulp.task('build', ["deleteDistFolder", 'copyGeneralFile', 'optimizeImages', 'useminTrigger']);
